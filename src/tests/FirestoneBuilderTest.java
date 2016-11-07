4946473567805e37fe9f13705e7663958b9517b9
Dina Jawad <dinaj@kth.se>
2016-11-07 16:23:22 +0100

    Add test folder

---
+++ src/tests/FirestoneBuilderTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirestoneBuilderTest {
+
+	@Test
+	public void testSetDeckIntStringArray() {
+	}
+
+	@Test
+	public void testSetMaxHealth() {
+	}
+
+	@Test
+	public void testSetStartingMana() {
+	}
+
+}
