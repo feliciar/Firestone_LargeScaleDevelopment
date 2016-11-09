1e92af1a0d1db407d5ff51969883e0718fc0e3d4
Dina Jawad <dinaj@kth.se>
2016-11-09 16:59:01 +0100

    Add turn functionality and communicate with game handlers

--- src/kth/firestone/FirestoneGame.java
+++ src/kth/firestone/FirestoneGame.java
@@ -1,16 +1,30 @@
 package kth.firestone;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
 
 import kth.firestone.card.Card;
+import kth.firestone.card.PlayCardHandler;
+import kth.firestone.deck.FirestoneDeck;
 import kth.firestone.minion.Minion;
 import kth.firestone.player.Player;
+import kth.firestone.player.GamePlayer;
 
 public class FirestoneGame implements Game {
-	List<Player> players;
+	private List<Player> players;
+	private List<Event> events;
+	private PlayCardHandler playCardHandler;
+	private AttackHandler attackHandler;
+	private int playerIndexInTurn = 1;	// first player starts by default
 	
-	public FirestoneGame(List<Player> players) {
+	public FirestoneGame(List<Player> players, 
+			PlayCardHandler playCardHandler,
+			AttackHandler attackHandler) {
 		this.players = players;
+		this.playCardHandler = playCardHandler;
+		this.attackHandler = attackHandler;
+		this.events = new ArrayList<>();
 	}
 	
 	@Override
@@ -20,74 +34,62 @@ public class FirestoneGame implements Game {
 
 	@Override
 	public Player getPlayerInTurn() {
-		// TODO Auto-generated method stub
-		return null;
+		return players.get(playerIndexInTurn-1);
 	}
 
 	@Override
 	public List<Event> playSpellCard(Player player, Card card) {
-		// TODO Auto-generated method stub
-		return null;
+		return playCardHandler.playSpellCard(player, card);
 	}
 
 	@Override
 	public List<Event> playSpellCard(Player player, Card card, String targetId) {
-		// TODO Auto-generated method stub
-		return null;
+		return playCardHandler.playSpellCard(player, card, targetId);
 	}
 
 	@Override
 	public List<Event> playMinionCard(Player player, Card card, int position) {
-		// TODO Auto-generated method stub
-		return null;
+		return playCardHandler.playMinionCard(player, card, position);
 	}
 
 	@Override
 	public List<Event> playMinionCard(Player player, Card card, int position, String targetId) {
-		// TODO Auto-generated method stub
-		return null;
+		return playCardHandler.playMinionCard(player, card, position, targetId);
 	}
 
 	@Override
 	public List<Event> playWeaponCard(Player player, Card card) {
-		// TODO Auto-generated method stub
-		return null;
+		return playCardHandler.playWeaponCard(player, card);
 	}
 
 	@Override
 	public List<Event> playWeaponCard(Player player, Card card, String targetId) {
-		// TODO Auto-generated method stub
-		return null;
+		return playCardHandler.playWeaponCard(player, card, targetId);
 	}
 
 	@Override
 	public boolean canAttack(Player player, Minion minion) {
-		// TODO Auto-generated method stub
-		return false;
+		return attackHandler.canAttack(player, minion);
 	}
 
 	@Override
 	public boolean isAttackValid(Player player, String attackerId, String targetId) {
-		// TODO Auto-generated method stub
-		return false;
+		return attackHandler.isAttackValid(player, attackerId, targetId);
 	}
 
 	@Override
 	public boolean isPlayCardValid(Player player, Card card) {
-		// TODO Auto-generated method stub
-		return false;
+		return false; //playCardHandler.isPlayCardValid(player, card);
 	}
 
 	@Override
 	public boolean isPlayCardValid(Player player, Card card, String targetId) {
-		// TODO Auto-generated method stub
-		return false;
+		return false;//playCardHandler.isPlayCardValid(player, card, targetId);
 	}
 
 	@Override
 	public List<Event> attack(Player player, String attackerId, String targetId) {
-		// TODO Auto-generated method stub
-		return null;
+		return attackHandler.attack(player, attackerId, targetId);
 	}
 
 	@Override
@@ -116,20 +118,43 @@ public class FirestoneGame implements Game {
 
 	@Override
 	public List<Event> endTurn(Player player) {
-		// TODO Auto-generated method stub
-		return null;
+		if (player.getId().equals("1")) {
+			playerIndexInTurn = 2;
+		} else {
+			playerIndexInTurn = 1;
+		}
+		// draw new card for next player
+		Card drawnCard = ((FirestoneDeck) player).getCards().pop();
+		List<Card> hand = player.getHand();
+		hand.add(drawnCard);
+		((GamePlayer) player).setHand(hand);
+		
+		return events;
 	}
 
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-
+		start(players.get(0));
+		
 	}
 
 	@Override
 	public void start(Player player) {
-		// TODO Auto-generated method stub
-
+		// deal cards at the start of the game
+		int nCardsToDeal = 4;
+		if (player.getId().equals("2")) {
+			nCardsToDeal = 3;
+		}
+		
+		List<Card> hand = new ArrayList<>();
+		ArrayDeque<Card> deck = ((FirestoneDeck) player.getDeck()).getCards();
+		if (player.getHand().size() != 0 || deck.size() < 4) {
+			System.exit(1);
+		}
+		for (int i = 0; i < nCardsToDeal; i++) {
+			hand.add(deck.pop());
+		}
+		((GamePlayer) player).setHand(hand);
 	}
 
 	@Override
