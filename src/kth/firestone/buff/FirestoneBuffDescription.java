a1e9c5c003b19babac4f0dd7244081550529754f
feliciar <feliciar@kth.se>
2016-11-17 13:39:46 +0100

    Create new class FirestoneBuffDescription

---
+++ src/kth/firestone/buff/FirestoneBuffDescription.java
@@ -0,0 +1,23 @@
+package kth.firestone.buff;
+
+public class FirestoneBuffDescription implements BuffDescription{
+	private String name;
+	private String description;
+	
+	public FirestoneBuffDescription(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
