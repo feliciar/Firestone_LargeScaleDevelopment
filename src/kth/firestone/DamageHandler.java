200ec2a0daa9d242d4b564c3991698733802fd95
hansdo <hansdo@kth.se>
2016-11-17 09:33:22 +0100

    Add a class DamageHandler and code to deal damage in attacks

---
+++ src/kth/firestone/DamageHandler.java
@@ -0,0 +1,36 @@
+package kth.firestone;
+
+import kth.firestone.hero.FirestoneHero;
+import kth.firestone.minion.FirestoneMinion;
+import kth.firestone.minion.Minion;
+import kth.firestone.player.Player;
+
+public class DamageHandler {
+
+	public void minionHeroAttack(FirestoneMinion attacker, FirestoneHero target){
+		target.reduceHealth(attacker.getAttack());
+		attacker.reduceHealth(target.getAttack());
+	}
+	
+	public void minionMinionAttack(FirestoneMinion attacker, FirestoneMinion target){
+		target.reduceHealth(attacker.getAttack());
+		attacker.reduceHealth(target.getAttack());
+	}
+	
+	public void removeTheDeadMinionMinionAttack(Minion attacker, Minion target, Player player, Player adversary){
+		if (target.getHealth() <= 0) {
+			adversary.getActiveMinions().remove(target);
+		}
+		if (attacker.getHealth() <= 0) {
+			player.getActiveMinions().remove(attacker);
+		}
+	}
+	
+	public boolean checkDefeat(){
+		//TODO check if a Hero is defeated
+		
+		return false;
+	}
+	
+	
+}
