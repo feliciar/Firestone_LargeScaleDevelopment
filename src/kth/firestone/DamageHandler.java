b7bbf54aff595642c409182af1dca218ce965a3b
hansdo <hansdo@kth.se>
2016-11-18 13:48:51 +0100

    Corrected misspelling and added some documentation

--- src/kth/firestone/DamageHandler.java
+++ src/kth/firestone/DamageHandler.java
@@ -7,26 +7,41 @@ import kth.firestone.player.Player;
 
 public class DamageHandler {
 
+	/*
+	 * Deals damage when a minion and hero battles.
+	 */
 	public void dealDamageToMinionAndHero(FirestoneMinion attacker, FirestoneHero target){
 		target.reduceHealth(attacker.getAttack());
 		attacker.reduceHealth(target.getAttack());
 	}
 	
+	/*
+	 * Deals damage to minions when two minions battle.
+	 */
 	public void dealDamageToMinions(FirestoneMinion attacker, FirestoneMinion target){
 		target.reduceHealth(attacker.getAttack());
 		attacker.reduceHealth(target.getAttack());
 	}
 	
+	/*
+	 * Deals specified amount of damage to a minion.
+	 */
 	public void dealDamageToOneMinion(FirestoneMinion minion, int damage) {
 		minion.reduceHealth(damage);
 	}
 	
-	public void dealOneDamageToSeveralMinion(FirestoneMinion... minions){
+	/*
+	 * Deals one damage to several minions.
+	 */
+	public void dealOneDamageToSeveralMinions(FirestoneMinion... minions){
 		for (FirestoneMinion m : minions){
 			m.reduceHealth(1);
 		}
 	}
 	
+	/*
+	 * Removes the dead minions from the board.
+	 */
 	public void removeDeadMinions(Minion attacker, Minion target, Player player, Player adversary){
 		if (target.getHealth() <= 0) {
 			adversary.getActiveMinions().remove(target);
