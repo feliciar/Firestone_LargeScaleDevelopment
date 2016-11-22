87eea47a63fb2404992ae5329f365fa1be57a332
hansdo <hansdo@kth.se>
2016-11-22 10:25:22 +0100

    Clean up input parameters

--- src/kth/firestone/DamageHandler.java
+++ src/kth/firestone/DamageHandler.java
@@ -4,48 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 import kth.firestone.hero.FirestoneHero;
+import kth.firestone.hero.Hero;
 import kth.firestone.minion.FirestoneMinion;
 import kth.firestone.minion.Minion;
-import kth.firestone.player.Player;
 
 public class DamageHandler {
 
 	/*
 	 * Deals damage when a minion and hero battles.
 	 */
-	public void dealDamageToMinionAndHero(FirestoneMinion attacker, FirestoneHero target){
-		target.reduceHealth(attacker.getAttack());
-		attacker.reduceHealth(target.getAttack());
+	public void dealDamageToMinionAndHero(Minion attacker, Hero target) {
+		((FirestoneHero) target).reduceHealth(attacker.getAttack());
+		((FirestoneMinion) attacker).reduceHealth(target.getAttack());
 	}
 	
 	/*
 	 * Deals damage to minions when two minions battle.
 	 */
-	public void dealDamageToMinions(FirestoneMinion attacker, FirestoneMinion target){
-		target.reduceHealth(attacker.getAttack());
-		attacker.reduceHealth(target.getAttack());
+	public void dealDamageToMinions(Minion attacker, Minion target) {
+		((FirestoneMinion) target).reduceHealth(attacker.getAttack());
+		((FirestoneMinion) attacker).reduceHealth(target.getAttack());
 	}
 	
 	/*
 	 * Deals specified amount of damage to a minion.
 	 */
-	public void dealDamageToOneMinion(FirestoneMinion minion, int damage) {
-		minion.reduceHealth(damage);
+	public void dealDamageToOneMinion(Minion minion, int damage) {
+		((FirestoneMinion) minion).reduceHealth(damage);
 	}
 	
 	/*
 	 * Deals one damage to several minions.
 	 */
-	public void dealOneDamageToSeveralMinions(ArrayList<FirestoneMinion> minions){
-		for (FirestoneMinion m : minions){
-			m.reduceHealth(1);
+	public void dealOneDamageToSeveralMinions(ArrayList<Minion> minions) {
+		for (Minion m : minions){
+			((FirestoneMinion) m).reduceHealth(1);
 		}
 	}
 	
 	/*
 	 * Help method to find all dead minions in a list.
 	 */
-	public List<Minion> findDeadMinions(List<Minion> minions){
+	public List<Minion> findDeadMinions(List<Minion> minions) {
 		List<Minion> deadMinions = new ArrayList<Minion>();
 		for (Minion m : minions){
 			if(m.getHealth() <= 0) {
@@ -58,7 +58,7 @@ public class DamageHandler {
 	/*
 	 * Removes all dead minions in a list of minions.
 	 */
-	public void removeDeadMinions(List<Minion> minions){
+	public void removeDeadMinions(List<Minion> minions) {
 		List<Minion> deadMinions = findDeadMinions(minions);
 		for (Minion dead : deadMinions){
 			minions.remove(dead);
