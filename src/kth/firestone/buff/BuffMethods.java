d7c9d6300e8d36e438d686162fa1471fbf4a6a8f
Dina Jawad <dinaj@kth.se>
2016-11-22 15:04:05 +0100

    Example method for testing

--- src/kth/firestone/buff/BuffMethods.java
+++ src/kth/firestone/buff/BuffMethods.java
@@ -13,11 +13,44 @@ import kth.firestone.player.Player;
 
 public class BuffMethods {
 	
+	DamageHandler damageHandler;
+	
 	public BuffMethods(DamageHandler damageHandler) {
-		
+		this.damageHandler = damageHandler;
 	}
 	
+	/**
+	 * Method for performing buff: "Battlecry: Deal 1 damage."
+	 */
+	public void battleCryDealOneDamage(List<Player> players, Player playerInTurn, 
+			Card playedCard, String attackerId, String targetId) {
+		
+		for (Player p : players) {
+			for (Minion m : p.getActiveMinions()) {
+				if (m.getId().equals(targetId)) {
+					damageHandler.dealDamageToOneMinion((FirestoneMinion) m, 1);
+				}
+			}
+			damageHandler.removeDeadMinions(p.getActiveMinions());
+		}
+	}
 	
-	
-	
+	/**
+	 * Method for performing buff: "After you cast a spell, deal 1 damage to ALL minions."
+	 */
+	/*public void afterSpellDealOneDamageToAllMinions(List<Player> players, Player playerInTurn, 
+			Card playedCard, String attackerId, String targetId) {
+		
+		if (playedCard.getType().equals(Card.Type.SPELL)) {
+			List<Minion> minionsToDealDamageTo = new ArrayList<>();
+			for (Player p : players) {
+				for (Minion m : p.getActiveMinions()) {
+					if (m.getName().equals(???))) continue;
+					minionsToDealDamageTo.add(m);
+				}
+			}
+			//damageHandler.dealOneDamageToSeveralMinions(minionsToDealDamageTo);
+			//TODO deal damage to all minions and remove dead minions
+		}
+	}	*/
 }
