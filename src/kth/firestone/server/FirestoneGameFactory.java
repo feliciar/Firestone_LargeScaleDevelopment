b1f714e8a64c99f99011da84160b136ce9481977
feliciar <feliciar@kth.se>
2016-12-03 09:28:40 +0100

    Randomized starting deck

--- src/kth/firestone/server/FirestoneGameFactory.java
+++ src/kth/firestone/server/FirestoneGameFactory.java
@@ -1,5 +1,11 @@
 package kth.firestone.server;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import kth.firestone.FirestoneBuilder;
 import kth.firestone.Game;
 import kth.firestone.GameBuilder;
@@ -19,13 +25,20 @@ public class FirestoneGameFactory implements GameFactory {
 		GameBuilder gb = new FirestoneBuilder(new GameData());
 		// set starting state
 		int startingMana = 0;
-		for (int i = 1; i < 3; i++) {
-			gb.setMaxHealth(i, 30)
-			  .setDeck(i, "Imp", "War Golem", "Boulderfist Ogre", 
+		String[] deckStrings = {"Imp", "War Golem", "Boulderfist Ogre", 
 				"Ironforge Rifleman", "Blackwing Corruptor", "Twilight Drake",
 				"Edwin VanCleef", "Midnight Drake", "Wild Pyromancer", "Frostbolt", "Shadowstep", 
 				"Backstab", "Equality", "Shadowform",
-				"Alexstrasza", "Acolyte of Pain", "Lorewalker Cho")
+				"Alexstrasza", "Acolyte of Pain", "Lorewalker Cho"};
+		List<List<String>> decks = new ArrayList<>();
+		decks.add(new ArrayList<>(Arrays.asList(deckStrings)));
+		decks.add(new ArrayList<>(Arrays.asList(deckStrings)));
+		Collections.shuffle(decks.get(0));
+		Collections.shuffle(decks.get(1));
+
+		for (int i = 1; i < 3; i++) {
+			gb.setMaxHealth(i, 30)
+			  .setDeck(i, decks.get(i-1))
 			  .setStartingMana(i, startingMana);
 		}
 		Game game = gb.build();
